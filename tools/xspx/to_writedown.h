1417693260593e97a66f9c3c87a23b66248f9c44
Mark Beckwith <mark@intrig.com>
2016-09-19 12:08:56 -0500

    prog with wd docs

--- tools/xspx/to_writedown.h
+++ tools/xspx/to_writedown.h
@@ -3,19 +3,62 @@
 
 namespace xspx {
 
+template <typename Elem>
+std::string attributes(const Elem & elem) {
+    std::ostringstream os;
+    if (elem.attributes.empty()) {
+        return "none";
+    }
+    os << "name  | format | description\n";
+    os << "------|--------|------------\n";
+    for (auto & a : elem.attributes) {
+        os << a.name << " | " << a.type_name << "|\n";
+    }
+
+    return os.str();
+}
+
+template <typename Elem>
+std::string children(const Elem & elem) {
+    auto v = elem.children;
+    if (v.empty()) return "none";
+    for (auto & s : v) {
+        s.insert(0, "<");
+        s.append(">");
+    }
+    return ict::join(v, ", ");
+}
+
+
+template <typename OS, typename Elem>
+void disp_element(OS & os, Elem const & elem) {
+    os << "# <" << elem.tag << "> element {\n\n";
+    
+    os << "attributes: " << attributes(elem) << "\n\n";
+
+    os << "children: " << children(elem) << "\n\n";
+
+
+    os << "}\n\n";
+}
+
 void to_writedown(std::ostream & os, const xsp_parser & xspx) {
+    os << ":title XDDL Element Reference\n\n";
+    os << ":toc(\"auto\")\n\n";
     for (auto & i : xspx.elems) {
         for (auto & j : i) {
-            if (!j.is_base) os << j.tag << ": " << j.name << '\n';
+            disp_element(os, j);
         }
-        os << "done\n";
+        os << "// done\n";
     }    
 
+#if 0
     os << "choices:\n";
     for (auto & choice : xspx.choices) {
         for (auto & i : choice.elems) os << i.tag << ": " << i.name << ", ";
         os << '\n';
     }
+#endif
 
 }
 
