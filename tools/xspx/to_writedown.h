290c6b708a869c68d784514a5107f61f103e7247
Mark Beckwith <mark@intrig.com>
2016-09-22 08:14:55 -0500

    updates

--- tools/xspx/to_writedown.h
+++ tools/xspx/to_writedown.h
@@ -7,12 +7,15 @@ template <typename Elem>
 std::string attributes(const Elem & elem) {
     std::ostringstream os;
     if (elem.attributes.empty()) {
-        return "none";
+        return "attributes: none\n";
     }
-    os << "name  | format | description\n";
-    os << "------|--------|------------\n";
+    os << "\n\nattributes | name  | format | required | description\n";
+    os <<     "-----------|-------|--------|----------|------------\n";
     for (auto & a : elem.attributes) {
-        os << a.name << " | " << a.type_name << "|\n";
+        os << " | " << a.name << " | " << a.type_name << "|";
+        if (a.required) os << " yes ";
+        os << " | ";
+        os << "\n";
     }
 
     return os.str();
@@ -23,8 +26,8 @@ std::string children(const Elem & elem) {
     auto v = elem.children;
     if (v.empty()) return "none";
     for (auto & s : v) {
-        s.insert(0, "<");
-        s.append(">");
+        s.insert(0, "&lt;");
+        s.append("&gt;");
     }
     return ict::join(v, ", ");
 }
@@ -32,9 +35,9 @@ std::string children(const Elem & elem) {
 
 template <typename OS, typename Elem>
 void disp_element(OS & os, Elem const & elem) {
-    os << "# <" << elem.tag << "> element {\n\n";
+    os << "# &lt;" << elem.tag << "&gt; {\n\n";
     
-    os << "attributes: " << attributes(elem) << "\n\n";
+    os << attributes(elem) << "\n\n";
 
     os << "children: " << children(elem) << "\n\n";
 
@@ -42,22 +45,28 @@ void disp_element(OS & os, Elem const & elem) {
     os << "}\n\n";
 }
 
+template <typename OS>
+void disp_choices(OS & os, const choice_type & choice) {
+    os << "# " <<  choice.name << "{\n";
+    for (auto & elem : choice.elems) disp_element(os, elem);
+    os << "}\n\n";
+}
+
 void to_writedown(std::ostream & os, const xsp_parser & xspx) {
     os << ":title XDDL Element Reference\n\n";
     os << ":toc(\"auto\")\n\n";
+    os << "# Elements {\n";
     for (auto & i : xspx.elems) {
         for (auto & j : i) {
-            disp_element(os, j);
+            if (!j.is_base) disp_element(os, j);
         }
-        os << "// done\n";
     }    
+    os << "} // Elements\n";
 
+    os << "# Polymorphic Elements {\n";
+    for (auto & choice : xspx.choices)disp_choices(os, choice); 
+    os << "} // Polymorphic Elements\n";
 #if 0
-    os << "choices:\n";
-    for (auto & choice : xspx.choices) {
-        for (auto & i : choice.elems) os << i.tag << ": " << i.name << ", ";
-        os << '\n';
-    }
 #endif
 
 }
